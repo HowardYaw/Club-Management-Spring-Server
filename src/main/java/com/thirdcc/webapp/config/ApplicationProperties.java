4bf7f7174d933f54f2cdaac4f5b706a16c39817e
Ka Chun <34387770+kachun333@users.noreply.github.com>
2021-03-07 22:05:49 +0800

    add accessToken and refreshToken in various profiles
    in dev, prod and test profile

--- src/main/java/com/thirdcc/webapp/config/ApplicationProperties.java
+++ src/main/java/com/thirdcc/webapp/config/ApplicationProperties.java
@@ -1,5 +1,6 @@
 package com.thirdcc.webapp.config;
 
+import io.github.jhipster.config.JHipsterProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
@@ -10,5 +11,64 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  */
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
 public class ApplicationProperties {
+
+    private final ApplicationProperties.AccessToken accessToken = new ApplicationProperties.AccessToken();
+    private final ApplicationProperties.RefreshToken refreshToken = new ApplicationProperties.RefreshToken();
+
+    public AccessToken getAccessToken() {
+        return accessToken;
+    }
+
+    public RefreshToken getRefreshToken() {
+        return refreshToken;
+    }
+
+    public static class AccessToken {
+        private String base64Secret = "";
+        private Long tokenValidityInSeconds = 0L;
+
+        public AccessToken() {
+        }
+
+        public String getBase64Secret() {
+            return base64Secret;
+        }
+
+        public void setBase64Secret(String base64Secret) {
+            this.base64Secret = base64Secret;
+        }
+
+        public Long getTokenValidityInSeconds() {
+            return tokenValidityInSeconds;
+        }
+
+        public void setTokenValidityInSeconds(Long tokenValidityInSeconds) {
+            this.tokenValidityInSeconds = tokenValidityInSeconds;
+        }
+    }
+
+    public static class RefreshToken {
+        private String base64Secret = "";
+        private Long tokenValidityInSeconds = 0L;
+
+        public RefreshToken() {
+        }
+
+        public String getBase64Secret() {
+            return base64Secret;
+        }
+
+        public void setBase64Secret(String base64Secret) {
+            this.base64Secret = base64Secret;
+        }
+
+        public Long getTokenValidityInSeconds() {
+            return tokenValidityInSeconds;
+        }
+
+        public void setTokenValidityInSeconds(Long tokenValidityInSeconds) {
+            this.tokenValidityInSeconds = tokenValidityInSeconds;
+        }
+    }
 
 }
