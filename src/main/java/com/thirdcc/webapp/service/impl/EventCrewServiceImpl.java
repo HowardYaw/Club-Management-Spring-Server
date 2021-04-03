25433094d1c8916fb189925b0c414dd412af720d
Clement <cskiat06@gmail.com>
2021-04-04 01:23:00 +0800

    Added test cases for UserResourceIT

--- src/main/java/com/thirdcc/webapp/service/impl/EventCrewServiceImpl.java
+++ src/main/java/com/thirdcc/webapp/service/impl/EventCrewServiceImpl.java
@@ -11,13 +11,11 @@ import com.thirdcc.webapp.repository.EventCrewRepository;
 import com.thirdcc.webapp.service.EventService;
 import com.thirdcc.webapp.service.dto.EventCrewDTO;
 import com.thirdcc.webapp.service.mapper.EventCrewMapper;
-import com.thirdcc.webapp.utils.PageUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
