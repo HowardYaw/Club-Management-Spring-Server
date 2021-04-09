86c5df42d90bb45ace51abf7e837d49268efc214
CarlosSia <siasimcheong@gmail.com>
2021-04-09 13:44:45 +0800

    get all open claim and update claim status

--- src/main/java/com/thirdcc/webapp/web/rest/ClaimResource.java
+++ src/main/java/com/thirdcc/webapp/web/rest/ClaimResource.java
@@ -1,5 +1,7 @@
 package com.thirdcc.webapp.web.rest;
 
+import com.thirdcc.webapp.domain.enumeration.ClaimStatus;
+import com.thirdcc.webapp.security.AuthoritiesConstants;
 import com.thirdcc.webapp.service.ClaimService;
 import com.thirdcc.webapp.web.rest.errors.BadRequestAlertException;
 import com.thirdcc.webapp.service.dto.ClaimDTO;
@@ -24,6 +26,7 @@ import java.net.URISyntaxException;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 /**
  * REST controller for managing {@link com.thirdcc.webapp.domain.Claim}.
@@ -46,47 +49,26 @@ public class ClaimResource {
     }
 
     /**
-     * {@code POST  /claims} : Create a new claim.
+     * {@code PUT  /claims} : Updates an existing claim status.
      *
-     * @param claimDTO the claimDTO to create.
-     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new claimDTO, or with status {@code 400 (Bad Request)} if the claim has already an ID.
-     * @throws URISyntaxException if the Location URI syntax is incorrect.
-     */
-    @PostMapping("/claims")
-    public ResponseEntity<ClaimDTO> createClaim(@RequestBody ClaimDTO claimDTO) throws URISyntaxException {
-        log.debug("REST request to save Claim : {}", claimDTO);
-        if (claimDTO.getId() != null) {
-            throw new BadRequestAlertException("A new claim cannot already have an ID", ENTITY_NAME, "idexists");
-        }
-        ClaimDTO result = claimService.save(claimDTO);
-        return ResponseEntity.created(new URI("/api/claims/" + result.getId()))
-            .headers(HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME, result.getId().toString()))
-            .body(result);
-    }
-
-    /**
-     * {@code PUT  /claims} : Updates an existing claim.
-     *
-     * @param claimDTO the claimDTO to update.
+     * @param id the id of the claim to update
+     * @param claimStatus the claimStatus to update to
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated claimDTO,
      * or with status {@code 400 (Bad Request)} if the claimDTO is not valid,
      * or with status {@code 500 (Internal Server Error)} if the claimDTO couldn't be updated.
-     * @throws URISyntaxException if the Location URI syntax is incorrect.
      */
-    @PutMapping("/claims")
-    public ResponseEntity<ClaimDTO> updateClaim(@RequestBody ClaimDTO claimDTO) throws URISyntaxException {
-        log.debug("REST request to update Claim : {}", claimDTO);
-        if (claimDTO.getId() == null) {
-            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
-        }
-        ClaimDTO result = claimService.save(claimDTO);
+    @PutMapping("/claims/{id}/status/{claimStatus}")
+    @PreAuthorize("hasRole(\"" + AuthoritiesConstants.ADMIN + "\") || @managementTeamSecurityExpression.isCurrentAdministrator()")
+    public ResponseEntity<ClaimDTO> updateClaimStatus(@PathVariable Long id, @PathVariable ClaimStatus claimStatus) {
+        log.debug("REST request to update claim: {} with status: {}", id, claimStatus);
+        ClaimDTO result = claimService.updateStatus(id, claimStatus);
         return ResponseEntity.ok()
-            .headers(HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, claimDTO.getId().toString()))
+            .headers(HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, id.toString()))
             .body(result);
     }
-
+    
     /**
-     * {@code GET  /claims} : get all the claims.
+     * {@code GET  /claims} : get all the claims with Open ClaimStatus.
      *
      * @param pageable the pagination information.
      * @param queryParams a {@link MultiValueMap} query parameters.
@@ -94,36 +76,11 @@ public class ClaimResource {
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of claims in body.
      */
     @GetMapping("/claims")
-    public ResponseEntity<List<ClaimDTO>> getAllClaims(Pageable pageable, @RequestParam MultiValueMap<String, String> queryParams, UriComponentsBuilder uriBuilder) {
+    @PreAuthorize("hasRole(\"" + AuthoritiesConstants.ADMIN + "\") || @managementTeamSecurityExpression.isCurrentAdministrator()")
+    public ResponseEntity<List<ClaimDTO>> getAllOpenClaims(Pageable pageable, @RequestParam MultiValueMap<String, String> queryParams, UriComponentsBuilder uriBuilder) {
         log.debug("REST request to get a page of Claims");
-        Page<ClaimDTO> page = claimService.findAll(pageable);
+        Page<ClaimDTO> page = claimService.findAllOpenClaims(pageable);
         HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(uriBuilder.queryParams(queryParams), page);
         return ResponseEntity.ok().headers(headers).body(page.getContent());
-    }
-
-    /**
-     * {@code GET  /claims/:id} : get the "id" claim.
-     *
-     * @param id the id of the claimDTO to retrieve.
-     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the claimDTO, or with status {@code 404 (Not Found)}.
-     */
-    @GetMapping("/claims/{id}")
-    public ResponseEntity<ClaimDTO> getClaim(@PathVariable Long id) {
-        log.debug("REST request to get Claim : {}", id);
-        Optional<ClaimDTO> claimDTO = claimService.findOne(id);
-        return ResponseUtil.wrapOrNotFound(claimDTO);
-    }
-
-    /**
-     * {@code DELETE  /claims/:id} : delete the "id" claim.
-     *
-     * @param id the id of the claimDTO to delete.
-     * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
-     */
-    @DeleteMapping("/claims/{id}")
-    public ResponseEntity<Void> deleteClaim(@PathVariable Long id) {
-        log.debug("REST request to delete Claim : {}", id);
-        claimService.delete(id);
-        return ResponseEntity.noContent().headers(HeaderUtil.createEntityDeletionAlert(applicationName, true, ENTITY_NAME, id.toString())).build();
     }
 }
