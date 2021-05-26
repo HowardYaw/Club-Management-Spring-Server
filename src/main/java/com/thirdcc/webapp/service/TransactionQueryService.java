8c607d5d2ce681941be588b8b4fd18e2749961da
Ka Chun <34387770+kachun333@users.noreply.github.com>
2021-05-26 21:47:35 +0800

    [Bug Fix] Fix Transaction Criteria

--- src/main/java/com/thirdcc/webapp/service/TransactionQueryService.java
+++ src/main/java/com/thirdcc/webapp/service/TransactionQueryService.java
@@ -6,8 +6,10 @@ import com.thirdcc.webapp.repository.TransactionRepository;
 import com.thirdcc.webapp.service.criteria.TransactionCriteria;
 import com.thirdcc.webapp.service.dto.TransactionDTO;
 import com.thirdcc.webapp.service.mapper.TransactionMapper;
+
 import java.util.List;
 import javax.persistence.criteria.JoinType;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
@@ -27,93 +29,109 @@ import tech.jhipster.service.QueryService;
 @Transactional(readOnly = true)
 public class TransactionQueryService extends QueryService<Transaction> {
 
-  private final Logger log = LoggerFactory.getLogger(TransactionQueryService.class);
+    private final Logger log = LoggerFactory.getLogger(TransactionQueryService.class);
 
-  private final TransactionRepository transactionRepository;
+    private final TransactionRepository transactionRepository;
 
-  private final TransactionMapper transactionMapper;
+    private final TransactionMapper transactionMapper;
 
-  public TransactionQueryService(TransactionRepository transactionRepository, TransactionMapper transactionMapper) {
-    this.transactionRepository = transactionRepository;
-    this.transactionMapper = transactionMapper;
-  }
-
-  /**
-   * Return a {@link List} of {@link TransactionDTO} which matches the criteria from the database.
-   * @param criteria The object which holds all the filters, which the entities should match.
-   * @return the matching entities.
-   */
-  @Transactional(readOnly = true)
-  public List<TransactionDTO> findByCriteria(TransactionCriteria criteria) {
-    log.debug("find by criteria : {}", criteria);
-    final Specification<Transaction> specification = createSpecification(criteria);
-    return transactionMapper.toDto(transactionRepository.findAll(specification));
-  }
-
-  /**
-   * Return a {@link Page} of {@link TransactionDTO} which matches the criteria from the database.
-   * @param criteria The object which holds all the filters, which the entities should match.
-   * @param page The page, which should be returned.
-   * @return the matching entities.
-   */
-  @Transactional(readOnly = true)
-  public Page<TransactionDTO> findByCriteria(TransactionCriteria criteria, Pageable page) {
-    log.debug("find by criteria : {}, page: {}", criteria, page);
-    final Specification<Transaction> specification = createSpecification(criteria);
-    return transactionRepository.findAll(specification, page).map(transactionMapper::toDto);
-  }
-
-  /**
-   * Return the number of matching entities in the database.
-   * @param criteria The object which holds all the filters, which the entities should match.
-   * @return the number of matching entities.
-   */
-  @Transactional(readOnly = true)
-  public long countByCriteria(TransactionCriteria criteria) {
-    log.debug("count by criteria : {}", criteria);
-    final Specification<Transaction> specification = createSpecification(criteria);
-    return transactionRepository.count(specification);
-  }
-
-  /**
-   * Function to convert {@link TransactionCriteria} to a {@link Specification}
-   * @param criteria The object which holds all the filters, which the entities should match.
-   * @return the matching {@link Specification} of the entity.
-   */
-  protected Specification<Transaction> createSpecification(TransactionCriteria criteria) {
-    Specification<Transaction> specification = Specification.where(null);
-    if (criteria != null) {
-      if (criteria.getId() != null) {
-        specification = specification.and(buildRangeSpecification(criteria.getId(), Transaction_.id));
-      }
-      if (criteria.getEventId() != null) {
-        specification = specification.and(buildRangeSpecification(criteria.getEventId(), Transaction_.eventId));
-      }
-      if (criteria.getReceiptId() != null) {
-        specification = specification.and(buildRangeSpecification(criteria.getReceiptId(), Transaction_.receiptId));
-      }
-      if (criteria.getType() != null) {
-        specification = specification.and(buildSpecification(criteria.getType(), Transaction_.type));
-      }
-      if (criteria.getAmount() != null) {
-        specification = specification.and(buildRangeSpecification(criteria.getAmount(), Transaction_.amount));
-      }
-      if (criteria.getDetails() != null) {
-        specification = specification.and(buildStringSpecification(criteria.getDetails(), Transaction_.details));
-      }
-      if (criteria.getCreatedBy() != null) {
-        specification = specification.and(buildStringSpecification(criteria.getCreatedBy(), Transaction_.createdBy));
-      }
-      if (criteria.getCreatedDate() != null) {
-        specification = specification.and(buildRangeSpecification(criteria.getCreatedDate(), Transaction_.createdDate));
-      }
-      if (criteria.getLastModifiedBy() != null) {
-        specification = specification.and(buildStringSpecification(criteria.getLastModifiedBy(), Transaction_.lastModifiedBy));
-      }
-      if (criteria.getLastModifiedDate() != null) {
-        specification = specification.and(buildRangeSpecification(criteria.getLastModifiedDate(), Transaction_.lastModifiedDate));
-      }
+    public TransactionQueryService(TransactionRepository transactionRepository, TransactionMapper transactionMapper) {
+        this.transactionRepository = transactionRepository;
+        this.transactionMapper = transactionMapper;
     }
-    return specification;
-  }
+
+    /**
+     * Return a {@link List} of {@link TransactionDTO} which matches the criteria from the database.
+     *
+     * @param criteria The object which holds all the filters, which the entities should match.
+     * @return the matching entities.
+     */
+    @Transactional(readOnly = true)
+    public List<TransactionDTO> findByCriteria(TransactionCriteria criteria) {
+        log.debug("find by criteria : {}", criteria);
+        final Specification<Transaction> specification = createSpecification(criteria);
+        return transactionMapper.toDto(transactionRepository.findAll(specification));
+    }
+
+    /**
+     * Return a {@link Page} of {@link TransactionDTO} which matches the criteria from the database.
+     *
+     * @param criteria The object which holds all the filters, which the entities should match.
+     * @param page     The page, which should be returned.
+     * @return the matching entities.
+     */
+    @Transactional(readOnly = true)
+    public Page<TransactionDTO> findByCriteria(TransactionCriteria criteria, Pageable page) {
+        log.debug("find by criteria : {}, page: {}", criteria, page);
+        final Specification<Transaction> specification = createSpecification(criteria);
+        return transactionRepository.findAll(specification, page).map(transactionMapper::toDto);
+    }
+
+    /**
+     * Return the number of matching entities in the database.
+     *
+     * @param criteria The object which holds all the filters, which the entities should match.
+     * @return the number of matching entities.
+     */
+    @Transactional(readOnly = true)
+    public long countByCriteria(TransactionCriteria criteria) {
+        log.debug("count by criteria : {}", criteria);
+        final Specification<Transaction> specification = createSpecification(criteria);
+        return transactionRepository.count(specification);
+    }
+
+    /**
+     * Function to convert {@link TransactionCriteria} to a {@link Specification}
+     *
+     * @param criteria The object which holds all the filters, which the entities should match.
+     * @return the matching {@link Specification} of the entity.
+     */
+    protected Specification<Transaction> createSpecification(TransactionCriteria criteria) {
+        Specification<Transaction> specification = Specification.where(null);
+        if (criteria != null) {
+            if (criteria.getId() != null) {
+                specification = specification.and(buildRangeSpecification(criteria.getId(), Transaction_.id));
+            }
+            if (criteria.getTitle() != null) {
+                specification = specification.and(buildStringSpecification(criteria.getTitle(), Transaction_.title));
+            }
+            if (criteria.getTransactionDate() != null) {
+                specification = specification.and(buildSpecification(criteria.getTransactionDate(), Transaction_.transactionDate));
+            }
+            if (criteria.getTransactionType() != null) {
+                specification = specification.and(buildSpecification(criteria.getTransactionType(), Transaction_.transactionType));
+            }
+            if (criteria.getTransactionStatus() != null) {
+                specification = specification.and(buildSpecification(criteria.getTransactionStatus(), Transaction_.transactionStatus));
+            }
+            if (criteria.getEventId() != null) {
+                specification = specification.and(buildRangeSpecification(criteria.getEventId(), Transaction_.eventId));
+            }
+            if (criteria.getTransactionAmount() != null) {
+                specification = specification.and(buildRangeSpecification(criteria.getTransactionAmount(), Transaction_.transactionAmount));
+            }
+            if (criteria.getImageLink() != null) {
+                specification = specification.and(buildStringSpecification(criteria.getImageLink(), Transaction_.imageLink));
+            }
+            if (criteria.getClosedBy() != null) {
+                specification = specification.and(buildStringSpecification(criteria.getClosedBy(), Transaction_.closedBy));
+            }
+            if (criteria.getDescription() != null) {
+                specification = specification.and(buildStringSpecification(criteria.getDescription(), Transaction_.description));
+            }
+            if (criteria.getCreatedBy() != null) {
+                specification = specification.and(buildStringSpecification(criteria.getCreatedBy(), Transaction_.createdBy));
+            }
+            if (criteria.getCreatedDate() != null) {
+                specification = specification.and(buildRangeSpecification(criteria.getCreatedDate(), Transaction_.createdDate));
+            }
+            if (criteria.getLastModifiedBy() != null) {
+                specification = specification.and(buildStringSpecification(criteria.getLastModifiedBy(), Transaction_.lastModifiedBy));
+            }
+            if (criteria.getLastModifiedDate() != null) {
+                specification = specification.and(buildRangeSpecification(criteria.getLastModifiedDate(), Transaction_.lastModifiedDate));
+            }
+        }
+        return specification;
+    }
 }
--- src/main/java/com/thirdcc/webapp/service/criteria/TransactionCriteria.java
+++ src/main/java/com/thirdcc/webapp/service/criteria/TransactionCriteria.java
@@ -1,8 +1,11 @@
 package com.thirdcc.webapp.service.criteria;
 
+import com.thirdcc.webapp.domain.enumeration.TransactionStatus;
 import com.thirdcc.webapp.domain.enumeration.TransactionType;
+
 import java.io.Serializable;
 import java.util.Objects;
+
 import tech.jhipster.service.Criteria;
 import tech.jhipster.service.filter.BigDecimalFilter;
 import tech.jhipster.service.filter.BooleanFilter;
@@ -25,257 +28,345 @@ import tech.jhipster.service.filter.StringFilter;
  */
 public class TransactionCriteria implements Serializable, Criteria {
 
-  /**
-   * Class for filtering TransactionType
-   */
-  public static class TransactionTypeFilter extends Filter<TransactionType> {
+    /**
+     * Class for filtering TransactionType
+     */
+    public static class TransactionTypeFilter extends Filter<TransactionType> {
 
-    public TransactionTypeFilter() {}
+        public TransactionTypeFilter() {
+        }
 
-    public TransactionTypeFilter(TransactionTypeFilter filter) {
-      super(filter);
+        public TransactionTypeFilter(TransactionTypeFilter filter) {
+            super(filter);
+        }
+
+        @Override
+        public TransactionTypeFilter copy() {
+            return new TransactionTypeFilter(this);
+        }
+    }
+
+    public static class TransactionStatusFilter extends Filter<TransactionStatus> {
+
+        public TransactionStatusFilter() {
+        }
+
+        public TransactionStatusFilter(TransactionStatusFilter filter) {
+            super(filter);
+        }
+
+        @Override
+        public TransactionStatusFilter copy() {
+            return new TransactionStatusFilter(this);
+        }
+    }
+
+    private static final long serialVersionUID = 1L;
+
+    private LongFilter id;
+
+    private StringFilter title;
+
+    private InstantFilter transactionDate;
+
+    private TransactionTypeFilter transactionType;
+
+    private TransactionStatusFilter transactionStatus;
+
+    private LongFilter eventId;
+
+    private BigDecimalFilter transactionAmount;
+
+    private StringFilter imageLink;
+
+    private StringFilter closedBy;
+
+    private StringFilter description;
+
+    private StringFilter createdBy;
+
+    private InstantFilter createdDate;
+
+    private StringFilter lastModifiedBy;
+
+    private InstantFilter lastModifiedDate;
+
+    public TransactionCriteria() {
+    }
+
+    public TransactionCriteria(TransactionCriteria other) {
+        this.id = other.id == null ? null : other.id.copy();
+        this.title = other.title == null ? null : other.title.copy();
+        this.transactionDate = other.transactionDate == null ? null : other.transactionDate.copy();
+        this.transactionType = other.transactionType == null ? null : other.transactionType.copy();
+        this.transactionStatus = other.transactionStatus == null ? null : other.transactionStatus.copy();
+        this.eventId = other.eventId == null ? null : other.eventId.copy();
+        this.transactionAmount = other.transactionAmount == null ? null : other.transactionAmount.copy();
+        this.closedBy = other.closedBy == null ? null : other.closedBy.copy();
+        this.createdBy = other.createdBy == null ? null : other.createdBy.copy();
+        this.createdDate = other.createdDate == null ? null : other.createdDate.copy();
+        this.lastModifiedBy = other.lastModifiedBy == null ? null : other.lastModifiedBy.copy();
+        this.lastModifiedDate = other.lastModifiedDate == null ? null : other.lastModifiedDate.copy();
     }
 
     @Override
-    public TransactionTypeFilter copy() {
-      return new TransactionTypeFilter(this);
+    public TransactionCriteria copy() {
+        return new TransactionCriteria(this);
     }
-  }
 
-  private static final long serialVersionUID = 1L;
-
-  private LongFilter id;
-
-  private LongFilter eventId;
-
-  private LongFilter receiptId;
-
-  private TransactionTypeFilter type;
-
-  private BigDecimalFilter amount;
-
-  private StringFilter details;
-
-  private StringFilter createdBy;
-
-  private InstantFilter createdDate;
-
-  private StringFilter lastModifiedBy;
-
-  private InstantFilter lastModifiedDate;
-
-  public TransactionCriteria() {}
-
-  public TransactionCriteria(TransactionCriteria other) {
-    this.id = other.id == null ? null : other.id.copy();
-    this.eventId = other.eventId == null ? null : other.eventId.copy();
-    this.receiptId = other.receiptId == null ? null : other.receiptId.copy();
-    this.type = other.type == null ? null : other.type.copy();
-    this.amount = other.amount == null ? null : other.amount.copy();
-    this.details = other.details == null ? null : other.details.copy();
-    this.createdBy = other.createdBy == null ? null : other.createdBy.copy();
-    this.createdDate = other.createdDate == null ? null : other.createdDate.copy();
-    this.lastModifiedBy = other.lastModifiedBy == null ? null : other.lastModifiedBy.copy();
-    this.lastModifiedDate = other.lastModifiedDate == null ? null : other.lastModifiedDate.copy();
-  }
-
-  @Override
-  public TransactionCriteria copy() {
-    return new TransactionCriteria(this);
-  }
-
-  public LongFilter getId() {
-    return id;
-  }
-
-  public LongFilter id() {
-    if (id == null) {
-      id = new LongFilter();
+    public LongFilter getId() {
+        return id;
     }
-    return id;
-  }
 
-  public void setId(LongFilter id) {
-    this.id = id;
-  }
-
-  public LongFilter getEventId() {
-    return eventId;
-  }
-
-  public LongFilter eventId() {
-    if (eventId == null) {
-      eventId = new LongFilter();
+    public LongFilter id() {
+        if (id == null) {
+            id = new LongFilter();
+        }
+        return id;
     }
-    return eventId;
-  }
 
-  public void setEventId(LongFilter eventId) {
-    this.eventId = eventId;
-  }
-
-  public LongFilter getReceiptId() {
-    return receiptId;
-  }
-
-  public LongFilter receiptId() {
-    if (receiptId == null) {
-      receiptId = new LongFilter();
+    public void setId(LongFilter id) {
+        this.id = id;
     }
-    return receiptId;
-  }
 
-  public void setReceiptId(LongFilter receiptId) {
-    this.receiptId = receiptId;
-  }
-
-  public TransactionTypeFilter getType() {
-    return type;
-  }
-
-  public TransactionTypeFilter type() {
-    if (type == null) {
-      type = new TransactionTypeFilter();
+    public StringFilter getTitle() {
+        return title;
     }
-    return type;
-  }
 
-  public void setType(TransactionTypeFilter type) {
-    this.type = type;
-  }
-
-  public BigDecimalFilter getAmount() {
-    return amount;
-  }
-
-  public BigDecimalFilter amount() {
-    if (amount == null) {
-      amount = new BigDecimalFilter();
+    public StringFilter title() {
+        if (title == null) {
+            title = new StringFilter();
+        }
+        return title;
     }
-    return amount;
-  }
 
-  public void setAmount(BigDecimalFilter amount) {
-    this.amount = amount;
-  }
-
-  public StringFilter getDetails() {
-    return details;
-  }
-
-  public StringFilter details() {
-    if (details == null) {
-      details = new StringFilter();
+    public void setTitle(StringFilter details) {
+        this.title = title;
     }
-    return details;
-  }
 
-  public void setDetails(StringFilter details) {
-    this.details = details;
-  }
-
-  public StringFilter getCreatedBy() {
-    return createdBy;
-  }
-
-  public StringFilter createdBy() {
-    if (createdBy == null) {
-      createdBy = new StringFilter();
+    public InstantFilter getTransactionDate() {
+        return transactionDate;
     }
-    return createdBy;
-  }
 
-  public void setCreatedBy(StringFilter createdBy) {
-    this.createdBy = createdBy;
-  }
-
-  public InstantFilter getCreatedDate() {
-    return createdDate;
-  }
-
-  public InstantFilter createdDate() {
-    if (createdDate == null) {
-      createdDate = new InstantFilter();
+    public InstantFilter transactionDate() {
+        if (transactionDate == null) {
+            transactionDate = new InstantFilter();
+        }
+        return transactionDate;
     }
-    return createdDate;
-  }
 
-  public void setCreatedDate(InstantFilter createdDate) {
-    this.createdDate = createdDate;
-  }
-
-  public StringFilter getLastModifiedBy() {
-    return lastModifiedBy;
-  }
-
-  public StringFilter lastModifiedBy() {
-    if (lastModifiedBy == null) {
-      lastModifiedBy = new StringFilter();
+    public void setTransactionDate(InstantFilter transactionDate) {
+        this.transactionDate = transactionDate;
     }
-    return lastModifiedBy;
-  }
 
-  public void setLastModifiedBy(StringFilter lastModifiedBy) {
-    this.lastModifiedBy = lastModifiedBy;
-  }
-
-  public InstantFilter getLastModifiedDate() {
-    return lastModifiedDate;
-  }
-
-  public InstantFilter lastModifiedDate() {
-    if (lastModifiedDate == null) {
-      lastModifiedDate = new InstantFilter();
+    public TransactionTypeFilter getTransactionType() {
+        return transactionType;
     }
-    return lastModifiedDate;
-  }
 
-  public void setLastModifiedDate(InstantFilter lastModifiedDate) {
-    this.lastModifiedDate = lastModifiedDate;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
+    public TransactionTypeFilter transactionType() {
+        if (transactionType == null) {
+            transactionType = new TransactionTypeFilter();
+        }
+        return transactionType;
     }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
+
+    public void setTransactionType(TransactionTypeFilter type) {
+        this.transactionType = type;
     }
-    final TransactionCriteria that = (TransactionCriteria) o;
-    return (
-      Objects.equals(id, that.id) &&
-      Objects.equals(eventId, that.eventId) &&
-      Objects.equals(receiptId, that.receiptId) &&
-      Objects.equals(type, that.type) &&
-      Objects.equals(amount, that.amount) &&
-      Objects.equals(details, that.details) &&
-      Objects.equals(createdBy, that.createdBy) &&
-      Objects.equals(createdDate, that.createdDate) &&
-      Objects.equals(lastModifiedBy, that.lastModifiedBy) &&
-      Objects.equals(lastModifiedDate, that.lastModifiedDate)
-    );
-  }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, eventId, receiptId, type, amount, details, createdBy, createdDate, lastModifiedBy, lastModifiedDate);
-  }
+    public TransactionStatusFilter getTransactionStatus() {
+        return transactionStatus;
+    }
 
-  // prettier-ignore
+    public TransactionStatusFilter transactionStatusFilter() {
+        if (transactionStatus == null) {
+            transactionStatus = new TransactionStatusFilter();
+        }
+        return transactionStatus;
+    }
+
+    public void setTransactionStatus(TransactionStatusFilter transactionStatus) {
+        this.transactionStatus = transactionStatus;
+    }
+
+    public LongFilter getEventId() {
+        return eventId;
+    }
+
+    public LongFilter eventId() {
+        if (eventId == null) {
+            eventId = new LongFilter();
+        }
+        return eventId;
+    }
+
+    public void setEventId(LongFilter eventId) {
+        this.eventId = eventId;
+    }
+
+    public BigDecimalFilter getTransactionAmount() {
+        return transactionAmount;
+    }
+
+    public BigDecimalFilter transactionAmount() {
+        if (transactionAmount == null) {
+            transactionAmount = new BigDecimalFilter();
+        }
+        return transactionAmount;
+    }
+
+    public void setTransactionAmount(BigDecimalFilter transactionAmount) {
+        this.transactionAmount = transactionAmount;
+    }
+
+    public StringFilter getImageLink() {
+        return imageLink;
+    }
+
+    public StringFilter imageLink() {
+        if (imageLink == null) {
+            imageLink = new StringFilter();
+        }
+        return imageLink;
+    }
+
+    public void setImageLink(StringFilter imageLink) {
+        this.imageLink = imageLink;
+    }
+
+    public StringFilter getClosedBy() {
+        return closedBy;
+    }
+
+    public StringFilter closedBy() {
+        if (closedBy == null) {
+            closedBy = new StringFilter();
+        }
+        return closedBy;
+    }
+
+    public void setClosedBy(StringFilter closedBy) {
+        this.closedBy = closedBy;
+    }
+
+
+    public StringFilter getDescription() {
+        return description;
+    }
+
+    public StringFilter description() {
+        if (title == null) {
+            title = new StringFilter();
+        }
+        return title;
+    }
+
+    public void setDescription(StringFilter description) {
+        this.description = description;
+    }
+
+    public StringFilter getCreatedBy() {
+        return createdBy;
+    }
+
+    public StringFilter createdBy() {
+        if (createdBy == null) {
+            createdBy = new StringFilter();
+        }
+        return createdBy;
+    }
+
+    public void setCreatedBy(StringFilter createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public InstantFilter getCreatedDate() {
+        return createdDate;
+    }
+
+    public InstantFilter createdDate() {
+        if (createdDate == null) {
+            createdDate = new InstantFilter();
+        }
+        return createdDate;
+    }
+
+    public void setCreatedDate(InstantFilter createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public StringFilter getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public StringFilter lastModifiedBy() {
+        if (lastModifiedBy == null) {
+            lastModifiedBy = new StringFilter();
+        }
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(StringFilter lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public InstantFilter getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public InstantFilter lastModifiedDate() {
+        if (lastModifiedDate == null) {
+            lastModifiedDate = new InstantFilter();
+        }
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(InstantFilter lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TransactionCriteria that = (TransactionCriteria) o;
+        return Objects.equals(id, that.id) &&
+            Objects.equals(title, that.title) &&
+            Objects.equals(transactionDate, that.transactionDate) &&
+            Objects.equals(transactionType, that.transactionType) &&
+            Objects.equals(transactionStatus, that.transactionStatus) &&
+            Objects.equals(eventId, that.eventId) &&
+            Objects.equals(transactionAmount, that.transactionAmount) &&
+            Objects.equals(closedBy, that.closedBy) &&
+            Objects.equals(description, that.description) &&
+            Objects.equals(createdBy, that.createdBy) &&
+            Objects.equals(createdDate, that.createdDate) &&
+            Objects.equals(lastModifiedBy, that.lastModifiedBy) &&
+            Objects.equals(lastModifiedDate, that.lastModifiedDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, transactionDate, transactionType, transactionStatus, eventId, transactionAmount, closedBy, description, createdBy, createdDate, lastModifiedBy, lastModifiedDate);
+    }
+
+    // prettier-ignore
     @Override
     public String toString() {
         return "TransactionCriteria{" +
-            (id != null ? "id=" + id + ", " : "") +
-            (eventId != null ? "eventId=" + eventId + ", " : "") +
-            (receiptId != null ? "receiptId=" + receiptId + ", " : "") +
-            (type != null ? "type=" + type + ", " : "") +
-            (amount != null ? "amount=" + amount + ", " : "") +
-            (details != null ? "details=" + details + ", " : "") +
-            (createdBy != null ? "createdBy=" + createdBy + ", " : "") +
-            (createdDate != null ? "createdDate=" + createdDate + ", " : "") +
-            (lastModifiedBy != null ? "lastModifiedBy=" + lastModifiedBy + ", " : "") +
-            (lastModifiedDate != null ? "lastModifiedDate=" + lastModifiedDate + ", " : "") +
-            "}";
+            "id=" + id +
+            ", title=" + title +
+            ", transactionDate=" + transactionDate +
+            ", transactionType=" + transactionType +
+            ", transactionStatus=" + transactionStatus +
+            ", eventId=" + eventId +
+            ", transactionAmount=" + transactionAmount +
+            ", closedBy=" + closedBy +
+            ", description=" + description +
+            ", createdBy=" + createdBy +
+            ", createdDate=" + createdDate +
+            ", lastModifiedBy=" + lastModifiedBy +
+            ", lastModifiedDate=" + lastModifiedDate +
+            '}';
     }
 }
